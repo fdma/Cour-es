011b24aa84afeca2ae42198104c5d397491a637c
Assanta2019 <60268496+Assanta2019@users.noreply.github.com>
2020-08-11 01:14:08 +0500

    Add files via upload
    1.0.2
    Уже частично рабочая версия! Реализовано добавление курсов и вывод этих же курсов во вкладке "Курсы"

--- routes/add.js
+++ routes/add.js
@@ -1,11 +1,20 @@
-const {Router} = require('express')
-const router = Router()
+const { Router } = require("express");
+const router = Router();
+const Course = require("../models/course");
 
-router.get('/', (req, res) => {
-  res.render('add', {
-    title: 'Добавить курс',
-    isAdd: true
-  })
-})
+router.get("/", (req, res) => {
+  res.render("add", {
+    title: "Добавить курс",
+    isAdd: true,
+  });
+});
 
-module.exports = router
+router.post("/", async (req, res) => {
+  const course = new Course(req.body.title, req.body.price, req.body.img);
+
+  await course.save();
+
+  res.redirect("/courses");
+});
+
+module.exports = router;
