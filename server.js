b3e3369530f5a064edbeb0d08155e699341d5d50
Asanta2019 <f.d.m.a@mail.ru>
2020-08-12 00:53:44 +0500

    1.0.3

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const PORT = 3001;
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/app", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
