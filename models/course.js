31d1601644d2a902931d4e3ed3711476cab01f07
Assanta2019 <60268496+Assanta2019@users.noreply.github.com>
2020-08-12 00:27:35 +0500

    Add files via upload
    1.0.3 Добавлено: теперь - Вы можете увидеть нажав на кнопку "Открыть курс" страницу курса. Исправлены баги.

--- models/course.js
+++ models/course.js
@@ -1,14 +1,13 @@
-const uuid = require("uuid");
-const fs = require("fs");
-const path = require("path");
-const { resolve } = require("path");
+const uuid = require('uuid/v4')
+const fs = require('fs')
+const path = require('path')
 
 class Course {
   constructor(title, price, img) {
-    this.title = title;
-    this.price = price;
-    this.img = img;
-    this.id = uuid;
+    this.title = title
+    this.price = price
+    this.img = img
+    this.id = uuid()
   }
 
   toJSON() {
@@ -16,43 +15,49 @@ class Course {
       title: this.title,
       price: this.price,
       img: this.img,
-      id: this.id,
-    };
+      id: this.id
+    }
   }
 
   async save() {
-    const courses = await Course.getAll();
-    courses.push(this.toJSON());
+    const courses = await Course.getAll()
+    courses.push(this.toJSON())
+
     return new Promise((resolve, reject) => {
       fs.writeFile(
-        path.join(__dirname, "..", "data", "courses.json"),
+        path.join(__dirname, '..', 'data', 'courses.json'),
         JSON.stringify(courses),
         (err) => {
           if (err) {
-            reject(err);
+            reject(err)
           } else {
-            resolve();
+            resolve()
           }
         }
-      );
-    });
+      )
+    })
   }
 
   static getAll() {
     return new Promise((resolve, reject) => {
       fs.readFile(
-        path.join(__dirname, "..", "data", "courses.json"),
-        "utf-8",
+        path.join(__dirname, '..', 'data', 'courses.json'),
+        'utf-8',
         (err, content) => {
           if (err) {
-            reject(err);
+            reject(err)
           } else {
-            resolve(JSON.parse(content));
+            resolve(JSON.parse(content))
           }
         }
-      );
-    });
+      )
+    })
+  }
+
+  static async getById(id) {
+    const courses = await Course.getAll()
+    return courses.find(c => c.id === id)
   }
 }
 
-module.exports = Course;
+module.exports = Course
